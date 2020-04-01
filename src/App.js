7d09ebdddadf61156f639381b5e06d79d5301e09
nonsondukwe <nonsondukwe24@gmail.com>
2020-04-01 16:00:13 +0100

    created navigation using withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 
-import Homepage from './pages/homepage/homepage.component';
+const HatPage = () => (
+<div>
+   
+ <h1>Hats Page</h1>
+
+</div>
+
+)
 
 function App() {
-  return <div><Homepage /></div>;
+  return <div>
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatPage} />
+    </Switch>
+  </div>;
   
     }
 
