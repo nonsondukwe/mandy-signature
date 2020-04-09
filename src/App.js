d8ee346d0d37a67db304b5bf47da286ad959968d
nonsondukwe <nonsondukwe24@gmail.com>
2020-04-09 13:40:09 +0100

    added redirect function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component';
@@ -42,13 +42,17 @@ class App extends React.Component {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
-      <Route path='/signin' component={SignInAndSignUpPage} />
+      <Route path='/signin' render={() => this.props.currentUser ? 
+      (<Redirect to ='/' />) : (<SignInAndSignUpPage />) } />
     </Switch>
     </div>;
  }
   
     }
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+});
 const mapDispatchedToProps = dispatch => ({setCurrentUser: user => 
     dispatch(setCurrentUser(user))});
 
-export default connect(null, mapDispatchedToProps )(App);
+export default connect( mapStateToProps, mapDispatchedToProps )(App);
